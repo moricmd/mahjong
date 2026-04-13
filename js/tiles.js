7d29462112295889e9f63c282615746b02c76f63
moricmd <rorsonik@gmail.com>
2026-04-13 11:41:40 +0900

    Update tiles.js

--- js/tiles.js
+++ js/tiles.js
@@ -33,3 +33,11 @@ export function generateTiles() {
 
   return tiles;
 }
+
+export function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
