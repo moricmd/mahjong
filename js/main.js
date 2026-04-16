80380008db32861334bbc89f93ba44aa189d6a6d
moricmd <rorsonik@gmail.com>
2026-04-16 13:41:09 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -21,6 +21,14 @@ autoSortBtn.addEventListener("click", () => {
   autoSort = !autoSort;
   autoSortBtn.textContent = `自動整理：${autoSort ? "ON" : "OFF"}`;
 
-  if (game) game.autoSort = autoSort;
+  if (game) {
+    game.autoSort = autoSort;
+
+    if (autoSort){
+      game.sortHand(0);
+      game.updateUI();
+    }
+
+  }
 });
 
