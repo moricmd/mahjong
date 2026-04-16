b996ebc3689bfee57827260ed6f16e321100d716
moricmd <rorsonik@gmail.com>
2026-04-16 13:23:46 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -10,3 +10,15 @@ window.addEventListener("load", () => {
     game.step();
   };
 });
+
+//
+//自動理牌
+//
+const autoSortBtn = document.getElementById("auto-sort-btn");
+let autoSort = false;
+
+autoSortBtn.addEventListener("click", () => {
+  autoSort = !autoSort;
+  autoSortBtn.textContent = `自動整理：${autoSort ? "ON" : "OFF"}`;
+});
+
