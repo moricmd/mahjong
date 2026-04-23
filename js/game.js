580b4b4176cc5ffc189b74853a82bba21172e19f
もり <rorsonik@gmail.com>
2026-04-23 12:04:18 +0900

    Update game.js

--- js/game.js
+++ js/game.js
@@ -162,10 +162,6 @@ export class Game {
   // -------------------------
   updateCPUHandPosition() {
 
-  // 確認用
-  console.log("CPU1 pos:", this.players[1].position);
-  console.log("CPU2 pos:", this.players[2].position);
-
   const cpu1 = this.players[1];
   const cpu2 = this.players[2];
 
